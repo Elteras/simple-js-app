afc556782a7b9c4e2d2ba76edec28e05d0e1b9e5
Elteras <b.m.schwartz92@googlemail.com>
2022-06-20 14:49:03 +0200

    Task 1.1
    Followed task instructions to:
    
    Assign pokemon + data to variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,24 @@
-alert('Hello world!');
+
+let Bulbasaur = {
+    name: 'Bulbasaur',
+    id: 001,
+    types: ['grass', 'poison']
+}
+
+let Charmander = {
+    name: 'Charmander',
+    id: 004,
+    types: ['fire']
+}
+
+let Squirtle = {
+    name: 'Squirtle',
+    id: 007,
+    types: ['water']
+    
+}
+
+let pokemonList = [Bulbasaur, Charmander, Squirtle]
+
+
+document.write(pokemonList[2].name)
