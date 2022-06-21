ced603a68e884c76252e62aa5d7f0eb9e7eda7bc
Elteras <b.m.schwartz92@googlemail.com>
2022-06-21 16:59:49 +0200

    Task 1.3
    Followed instructions for exercise & task 1.3. This included adding a for loop to list the pokemon and identify any over a certain size.

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,20 @@
 let Bulbasaur = {
     name: 'Bulbasaur',
     id: 001,
+    height: 0.7,
     types: ['grass', 'poison']
 }
 
 let Charmander = {
     name: 'Charmander',
+    height: 0.6,
     id: 004,
     types: ['fire']
 }
 
 let Squirtle = {
     name: 'Squirtle',
+    height: 0.5,
     id: 007,
     types: ['water']
     
@@ -20,5 +23,10 @@ let Squirtle = {
 
 let pokemonList = [Bulbasaur, Charmander, Squirtle]
 
-
-document.write(pokemonList[2].name)
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height > 0.6) { 
+        document.write('<p>' + pokemonList[i].name + ' (Height: ' + pokemonList[i].height + ')' + ' - chonky boi!' + '</p>')
+}else{
+        document.write('<p>' + pokemonList[i].name + ' (Height: ' + pokemonList[i].height + ')' + '</p>')
+    }
+}
